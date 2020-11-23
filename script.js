d81cb60fdb4d8cc39b430f12dbbf343ec4112ddc
Ben Pabian <benjamin.pabian@hotmail.com>
2020-11-23 17:02:30 +1100

    Added final comments

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
+//Outlining all of the buttons I'll be listening to
 let counter = document.querySelector("#currentNo");
 const minusBtn = document.querySelector("#minusNo");
 const addBtn = document.querySelector("#addNo");
 const resetBtn = document.querySelector("#reset");
 
+//Grabbing the info for the background
 const bodyBg = document.querySelector("body");
 
+//Creating a counter to edit/change
 let count = 0;
 
 //Event listener to listen for when the button is pressed. When it's pressed, the function is ran.
@@ -12,6 +15,7 @@ minusNo.addEventListener('click', minusCounter);
 addNo.addEventListener('click', addCounter);
 reset.addEventListener('click', resetCounter);
 
+//Adding 1 to the count, then calling currentColor
 function addCounter() {
 	count++;
 	counter.innerHTML = count;
@@ -19,6 +23,7 @@ function addCounter() {
 	currentColor();
 }
 
+//Removing 1 to the count, then calling currentColor
 function minusCounter() {
 	count--;
 	counter.innerHTML = count;
@@ -26,6 +31,7 @@ function minusCounter() {
 	currentColor();	
 }
 
+//Resetting the count, then calling currentColor
 function resetCounter() {
 	count = 0;
 	counter.innerHTML = count;
@@ -33,6 +39,8 @@ function resetCounter() {
 	currentColor();	
 }
 
+//Checks the current number and depending on that result, changes the background
+//Also commented out code that'd only change the colour of the number itself, so I don't give myself a seizure whilst testing
 function currentColor() {
 	//bodyBg.style.backgroundColor = count;
 
