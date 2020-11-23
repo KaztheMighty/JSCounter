b9da56be5dceb495e754d56a5d8e76e9e109ed71
Ben Pabian <benjamin.pabian@hotmail.com>
2020-11-23 16:39:15 +1100

    Outlining of Project

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-const hexBtn = document.getElementById("hexButton");
+const minus = document.addEventListener("minusButton");
+const add = document.addEventListener("addButton");
 
 const bodyBg = document.querySelector("body");
-const hex = document.querySelector(".hex");
+const hex = document.querySelector(".currentNo");
 
 //Event listener to listen for when the button is pressed. When it's pressed, the function is ran.
 hexBtn.addEventListener('click', updateNo);
 
-function updateNo() {
+function addNo() {
 	hex.innerHTML = hexCol;
 	updateCurrentNo();
 }
