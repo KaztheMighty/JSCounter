d2ceb83aac5a1755571aad97f79d398170c80500
Ben Pabian <benjamin.pabian@hotmail.com>
2020-11-23 16:59:33 +1100

    Final Project

--- script.js
+++ script.js
@@ -1,17 +1,53 @@
-const minus = document.addEventListener("minusButton");
-const add = document.addEventListener("addButton");
+let counter = document.querySelector("#currentNo");
+const minusBtn = document.querySelector("#minusNo");
+const addBtn = document.querySelector("#addNo");
+const resetBtn = document.querySelector("#reset");
 
 const bodyBg = document.querySelector("body");
-const hex = document.querySelector(".currentNo");
+
+let count = 0;
 
 //Event listener to listen for when the button is pressed. When it's pressed, the function is ran.
-hexBtn.addEventListener('click', updateNo);
+minusNo.addEventListener('click', minusCounter);
+addNo.addEventListener('click', addCounter);
+reset.addEventListener('click', resetCounter);
 
-function addNo() {
-	hex.innerHTML = hexCol;
-	updateCurrentNo();
+function addCounter() {
+	count++;
+	counter.innerHTML = count;
+
+	currentColor();
 }
 
-function updateCurrentNo() {
-	bodyBg.style.backgroundColor = hexCol;
+function minusCounter() {
+	count--;
+	counter.innerHTML = count;
+
+	currentColor();	
+}
+
+function resetCounter() {
+	count = 0;
+	counter.innerHTML = count;
+
+	currentColor();	
+}
+
+function currentColor() {
+	//bodyBg.style.backgroundColor = count;
+
+	if (counter.innerHTML > "0"){
+		bodyBg.style.backgroundColor = "green";
+		//counter.style.color = "green";
+	}
+
+	if (counter.innerHTML < "0") {
+		bodyBg.style.backgroundColor = "red";
+		//counter.style.color = "red";
+	}
+
+	if (counter.innerHTML === "0") {
+		bodyBg.style.backgroundColor = "white";
+		//counter.style.color = "black";
+	}
 }
